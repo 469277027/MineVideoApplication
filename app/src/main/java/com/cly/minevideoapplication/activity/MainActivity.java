5bc636164cd0e559c1f22bd9ea3c8d7236f27a9d
Angel <conglongyu@vip.qq.com>
2017-05-15 19:51:24 +0800

    first commit

---
+++ app/src/main/java/com/cly/minevideoapplication/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cly.minevideoapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cly.minevideoapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
